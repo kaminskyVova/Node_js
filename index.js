2c8965a51ddf068c23c96ead80dadcbdf9866da0
Kaminsky <kaminskyfrontend@gmail.com>
2021-04-28 03:26:36 +0300

    hw-1

---
+++ index.js
@@ -0,0 +1,8 @@
+const sqlite = require("sqlite3")
+const postgre = require("pg")
+const fs = require("fs")
+
+
+console.log("Использую gitHub норм?");
+
+fs.writeFileSync("work.txt", "It`s work!!!")
